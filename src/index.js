c345fc352b2bb30ff67ef9edd16aff7a47a60d69
AndreyPodvigin <sn.podvigin.andrey@gmail.com>
2022-05-21 14:38:38 +0300

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const oddLis = document.querySelectorAll('.item:nth-child(odd)');
+  for (let element of oddLis) {
+    element.style.backgroundColor = 'red';
+  };
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  const allLis = document.querySelectorAll('.item');
+  let firstLi = document.querySelector('ul > li:first-child');
+  for (let i = 0; i < allLis.length; i++) {
+    let nextLi = firstLi.nextElementSibling;
+    if (nextLi.matches('.item.likedItem')) {
+      nextLi.style.backgroundColor = 'blue';
+    } else {
+      firstLi = nextLi;
+    }
+  }
+};
